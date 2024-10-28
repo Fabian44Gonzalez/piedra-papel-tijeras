b9a847eaf42114d933a1f65db5fb6866fb9c0419
Fabián González Rodríguez <fabian205gonzlez@gmail.com>
2024-10-28 15:00:43 +0100

    eleccion jugador creada

--- mian.py
+++ mian.py
@@ -7,5 +7,21 @@ def menu():
 
 menu()
 
+def eleccion_jugador():
+    jugador = -1
+
+    while jugador <= -1:
+        jugador = int(input("Escoge tu elección"))
+        if jugador == 0:
+            print("Piedra")
+        elif jugador == 1:
+            print("Papel")
+        elif jugador == 2:
+            print("Tijeras")
+        else:
+            print("Escoge un valor correcto")
+
+eleccion_jugador()
+
 
 
